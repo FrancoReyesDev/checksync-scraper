03fcceed04c013fe3c33217cab94033eac2d838a
la pesadilla <fdr160102@gmail.com>
2024-06-07 22:31:55 -0300

    scraper done

--- src/scraper/mercadoPago/index.ts
+++ src/scraper/mercadoPago/index.ts
@@ -1,121 +1,123 @@
-import puppeteer, {Browser, Cookie} from 'puppeteer';
-import {MercadoPagoScraper} from './types';
-import {runner} from './runner';
-import {loginWithCookies} from 'src/lib/loginWithCookies';
-import {loginHandler} from 'src/lib/loginHandler';
+import puppeteer, { Browser, Cookie } from "puppeteer";
+import { MercadoPagoScraper } from "./types";
+import { runner } from "./runner";
+import { loginWithCookies } from "src/lib/loginWithCookies";
+import { loginHandler } from "src/lib/loginHandler";
 
 export const mercadoPagoScraper: MercadoPagoScraper = ({
-	findMovement,
-	setMovements,
+  findMovement,
+  setMovements,
 }) => {
-	const config = {
-		name: 'mercado pago',
-		color: '',
-		loginUrl: 'https://www.mercadolibre.com/jms/mla/lgz/login?platform_id=MP',
-		loggedOrigin: 'https://www.mercadopago.com.ar',
-		loggedInPathHint: 'home',
-		scrap: {
-			frequency: 1800000,
-			startFromId: '75279230369',
-			maxPage: 10,
-		},
-	};
+  const config = {
+    name: "mercado pago",
+    color: "",
+    loginUrl: "https://www.mercadolibre.com/jms/mla/lgz/login?platform_id=MP",
+    loggedOrigin: "https://www.mercadopago.com.ar",
+    loggedInPathHint: "home",
+    scrap: {
+      frequency: 1800000,
+      startFromId: "75279230369",
+      maxPage: 10,
+    },
+  };
 
-	let workingInterval: NodeJS.Timeout | null = null;
-	let browser: Browser | null = null;
+  let workingInterval: NodeJS.Timeout | null = null;
+  let browser: Browser | null = null;
 
-	const status = {
-		isLoggedIn: false,
-		isWorking: false,
-	};
+  const status = {
+    isLoggedIn: false,
+    isWorking: false,
+  };
 
-	let sessionCookies: Cookie[] = [];
+  let sessionCookies: Cookie[] = [];
 
-	const loginClientHandler: ReturnType<MercadoPagoScraper>['loginClientHandler'] =
-		() => {
-			const {login, close} = loginHandler({
-				loggedPathHint: config.loggedInPathHint,
-				loginUrl: config.loginUrl,
-			});
+  const loginClientHandler: ReturnType<MercadoPagoScraper>["loginClientHandler"] =
+    () => {
+      const { login, close } = loginHandler({
+        loggedPathHint: config.loggedInPathHint,
+        loginUrl: config.loginUrl,
+      });
 
-			const handleLogin = async () => {
-				const loginAttempt = await login();
-				if ('cookies' in loginAttempt) {
-					sessionCookies = loginAttempt.cookies;
-					return true;
-				}
-				return false;
-			};
+      const handleLogin = async () => {
+        const loginAttempt = await login();
+        if ("cookies" in loginAttempt) {
+          sessionCookies = loginAttempt.cookies;
+          return true;
+        }
+        return false;
+      };
 
-			return {login: handleLogin, close};
-		};
+      return { login: handleLogin, close };
+    };
 
-	const login: ReturnType<MercadoPagoScraper>['login'] =
-		async newSessionCookies => {
-			sessionCookies = newSessionCookies;
-			status.isLoggedIn = true;
-		};
+  const login: ReturnType<MercadoPagoScraper>["login"] = async (
+    newSessionCookies
+  ) => {
+    sessionCookies = newSessionCookies;
+    status.isLoggedIn = true;
+  };
 
-	const start = () => {
-		if (
-			status.isLoggedIn === false ||
-			workingInterval !== null ||
-			browser !== null
-		)
-			finish();
-		workingInterval = setInterval(scrap, config.scrap.frequency);
-	};
+  const start = () => {
+    if (
+      status.isLoggedIn === false ||
+      workingInterval !== null ||
+      browser !== null
+    )
+      finish();
+    workingInterval = setInterval(scrap, config.scrap.frequency);
+  };
 
-	const logout = () => {
-		sessionCookies = [];
-		status.isLoggedIn = false;
-	};
+  const logout = () => {
+    sessionCookies = [];
+    status.isLoggedIn = false;
+  };
 
-	const scrap = async (visible: boolean = false) => {
-		if (sessionCookies.length === 0) return;
+  const scrap = async (visible: boolean = false) => {
+    if (sessionCookies.length === 0) return;
 
-		if (browser === null)
-			browser = await puppeteer.launch({headless: !visible});
-		const page = await loginWithCookies({
-			loggedOrigin: config.loggedOrigin,
-			sessionCookies,
-			browser,
-		});
-		const movements = await runner({
-			page,
-			browser,
-			findMovement,
-			startFromId: config.scrap.startFromId,
-			maxPage: config.scrap.maxPage,
-		});
+    const browser = await puppeteer.launch({ headless: !visible });
+    const page = await loginWithCookies({
+      loggedOrigin: config.loggedOrigin,
+      sessionCookies,
+      browser,
+    });
+    const movements = await runner({
+      page,
+      browser,
+      findMovement,
+      startFromId: config.scrap.startFromId,
+      maxPage: config.scrap.maxPage,
+    });
 
-		setMovements(movements);
-	};
+    await browser.close();
 
-	const finish = () => {
-		if (
-			status.isLoggedIn === false ||
-			(workingInterval === null && browser === null)
-		)
-			return;
+    setMovements(movements);
+  };
 
-		clearTimeout(workingInterval as NodeJS.Timeout);
-		browser = null;
-	};
+  const finish = () => {
+    if (
+      status.isLoggedIn === false ||
+      (workingInterval === null && browser === null)
+    )
+      return;
 
-	const getConfig = () => config;
-	const getStatus = () => status;
-	const getSessionCookies = () => sessionCookies;
+    clearTimeout(workingInterval as NodeJS.Timeout);
+    browser = null;
+  };
 
-	return {
-		getConfig,
-		getStatus,
-		getSessionCookies,
-		login,
-		loginClientHandler,
-		logout,
-		start,
-		finish,
-		scrap,
-	};
+  const getConfig = () => config;
+  const getStatus = () => status;
+  const getSessionCookies = () => sessionCookies;
+
+  return {
+    getConfig,
+    getStatus,
+    getSessionCookies,
+    login,
+    loginClientHandler,
+    logout,
+    start,
+    finish,
+    scrap,
+  };
 };
--- src/scraper/mercadoPago/runner.ts
+++ src/scraper/mercadoPago/runner.ts
@@ -1,112 +1,114 @@
-import {Browser, Page} from 'puppeteer';
+import { Browser, Page } from "puppeteer";
 import {
-	DetailedMovement,
-	MercadoPagoScraperDependencies,
-	Movement,
-} from './types';
+  DetailedMovement,
+  MercadoPagoScraperDependencies,
+  Movement,
+} from "./types";
 
 const goToActivities = async ({
-	page,
-	pageNumber,
+  page,
+  pageNumber,
 }: {
-	page: Page;
-	pageNumber: number;
+  page: Page;
+  pageNumber: number;
 }) => {
-	await page.goto('https://www.mercadopago.com.ar/activities/' + pageNumber, {
-		waitUntil: 'networkidle0',
-	});
+  await page.goto("https://www.mercadopago.com.ar/activities/" + pageNumber, {
+    waitUntil: "networkidle0",
+  });
 };
 
-const getFinalPageNumberOnActivities = async ({page}: {page: Page}) => {
-	await goToActivities({page, pageNumber: 1});
-	const paginationButton = 'li.andes-pagination__button';
+const getFinalPageNumberOnActivities = async ({ page }: { page: Page }) => {
+  await goToActivities({ page, pageNumber: 1 });
+  const paginationButton = "li.andes-pagination__button";
 
-	await page.waitForSelector(paginationButton);
-	return await page.$$eval(paginationButton, buttons => {
-		const getBeforeLastButton =
-			buttons[buttons.length - 2] ?? buttons[buttons.length - 1];
-		return getBeforeLastButton !== undefined
-			? Number(getBeforeLastButton.textContent) ?? 1
-			: 1;
-	});
+  await page.waitForSelector(paginationButton);
+  return await page.$$eval(paginationButton, (buttons) => {
+    const getBeforeLastButton =
+      buttons[buttons.length - 2] ?? buttons[buttons.length - 1];
+    return getBeforeLastButton !== undefined
+      ? Number(getBeforeLastButton.textContent) ?? 1
+      : 1;
+  });
 };
 
-const getPageMovements = async ({page}: {page: Page}) => {
-	const label = 'a.ui-row__link';
-	const negativeSymbol = 'andes-money-amount__negative-symbol';
-	const amountFraction = 'andes-money-amount__fraction';
-	const amountCents = 'andes-money-amount__cents';
-	const formatAmount = (fraction: string, cents: string) =>
-		Number(`${fraction.replaceAll('.', '')}.${cents}`);
+const getPageMovements = async ({ page }: { page: Page }) => {
+  const label = "a.ui-row__link";
+  const negativeSymbol = "andes-money-amount__negative-symbol";
+  const amountFraction = "andes-money-amount__fraction";
+  const amountCents = "andes-money-amount__cents";
+  const formatAmount = (fraction: string, cents: string) =>
+    Number(`${fraction.replaceAll(".", "")}.${cents}`);
 
-	const movements = await page.$$eval(
-		label,
-		(movement, negativeSymbol, amountCents, amountFraction) =>
-			movement
-				.filter(a => a.getElementsByClassName(negativeSymbol)[0] === undefined)
-				.map(a => {
-					const url = a.href;
-					const date = a.getElementsByTagName('time')[0]!.textContent!;
-					const fraction =
-						a.getElementsByClassName(amountFraction)[0]!.textContent!;
-					const cents = a.getElementsByClassName(amountCents)[0]!.textContent!;
+  const movements = await page.$$eval(
+    label,
+    (movement, negativeSymbol, amountCents, amountFraction) =>
+      movement
+        .filter(
+          (a) => a.getElementsByClassName(negativeSymbol)[0] === undefined
+        )
+        .map((a) => {
+          const url = a.href;
+          const date = a.getElementsByTagName("time")[0]!.textContent!;
+          const fraction =
+            a.getElementsByClassName(amountFraction)[0]!.textContent!;
+          const cents = a.getElementsByClassName(amountCents)[0]!.textContent!;
 
-					return {url, date, fraction, cents};
-				}),
-		negativeSymbol,
-		amountCents,
-		amountFraction,
-	);
+          return { url, date, fraction, cents };
+        }),
+    negativeSymbol,
+    amountCents,
+    amountFraction
+  );
 
-	return movements.map(({url, date, fraction, cents}) => ({
-		url,
-		date,
-		amount: formatAmount(fraction, cents),
-	}));
+  return movements.map(({ url, date, fraction, cents }) => ({
+    url,
+    date,
+    amount: formatAmount(fraction, cents),
+  }));
 };
 
 const openMovement = async ({
-	browser,
-	url,
+  browser,
+  url,
 }: {
-	browser: Browser;
-	url: string;
+  browser: Browser;
+  url: string;
 }) => {
-	const page = await browser.newPage();
-	await page.goto(url, {waitUntil: 'networkidle0'});
-	return page;
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "networkidle0" });
+  return page;
 };
 
 const movementClassSelectors = {
-	id: 'span.c-copy-operation__text--initial',
-	type: 'div.ticket-v2-row__title',
-	status: 'div.andes-message__content',
-	userName: 'li.user-info-v2__name',
-	userDetails: 'li.user-info-v2__detail',
+  id: "span.c-copy-operation__text--initial",
+  type: "div.ticket-v2-row__title",
+  status: "div.andes-message__content",
+  userName: "li.user-info-v2__name",
+  userDetails: "li.user-info-v2__detail",
 };
 
-const waitForIdSelector = async ({page}: {page: Page}) => {
-	const {id} = movementClassSelectors;
-	return page
-		.waitForSelector(id, {
-			timeout: 2000,
-		})
-		.then(() => true)
-		.catch(() => false);
+const waitForIdSelector = async ({ page }: { page: Page }) => {
+  const { id } = movementClassSelectors;
+  return page
+    .waitForSelector(id, {
+      timeout: 2000,
+    })
+    .then(() => true)
+    .catch(() => false);
 };
 
-const retryOnError = async ({page}: {page: Page}) => {
-	for (let attempt = 1; attempt <= 2; attempt++) {
-		console.log('reintento ' + attempt);
-		try {
-			await page.reload({waitUntil: 'networkidle0'});
-			const pageLoadedCorrectly = await waitForIdSelector({page});
-			if (pageLoadedCorrectly) return true;
-		} catch (error) {
-			console.error(`Error on attempt ${attempt} to reload page:`, error);
-			if (attempt === 2) return false;
-		}
-	}
+const retryOnError = async ({ page }: { page: Page }) => {
+  for (let attempt = 1; attempt <= 2; attempt++) {
+    console.log("reintento " + attempt);
+    try {
+      await page.reload({ waitUntil: "networkidle0" });
+      const pageLoadedCorrectly = await waitForIdSelector({ page });
+      if (pageLoadedCorrectly) return true;
+    } catch (error) {
+      console.error(`Error on attempt ${attempt} to reload page:`, error);
+      if (attempt === 2) return false;
+    }
+  }
 };
 
 // const retryOnError = async ({page}: {page: Page}) => {
@@ -118,182 +120,185 @@ const retryOnError = async ({page}: {page: Page}) => {
 // 	}
 // };
 
-const waitForSelectors = async ({page}: {page: Page}) => {
-	const {type, status, userName, userDetails} = movementClassSelectors;
+const waitForSelectors = async ({ page }: { page: Page }) => {
+  const { type, status, userName, userDetails } = movementClassSelectors;
 
-	let statusDoesntExist = false;
-	let userInfoDoesntExist = false;
-	let typeDoesntExist = false;
+  let statusDoesntExist = false;
+  let userInfoDoesntExist = false;
+  let typeDoesntExist = false;
 
-	await Promise.all([
-		page
-			.waitForSelector(type, {timeout: 2000})
-			.catch(() => (typeDoesntExist = true)),
-		page
-			.waitForSelector(status, {timeout: 2000})
-			.catch(() => (statusDoesntExist = true)),
-		page
-			.waitForSelector(userName, {timeout: 2000})
-			.catch(() => (userInfoDoesntExist = true)),
-		page
-			.waitForSelector(userDetails, {timeout: 2000})
-			.catch(() => (userInfoDoesntExist = true)),
-	]);
+  await Promise.all([
+    page
+      .waitForSelector(type, { timeout: 2000 })
+      .catch(() => (typeDoesntExist = true)),
+    page
+      .waitForSelector(status, { timeout: 2000 })
+      .catch(() => (statusDoesntExist = true)),
+    page
+      .waitForSelector(userName, { timeout: 2000 })
+      .catch(() => (userInfoDoesntExist = true)),
+    page
+      .waitForSelector(userDetails, { timeout: 2000 })
+      .catch(() => (userInfoDoesntExist = true)),
+  ]);
 
-	return {
-		statusDoesntExist,
-		userInfoDoesntExist,
-		typeDoesntExist,
-	};
+  return {
+    statusDoesntExist,
+    userInfoDoesntExist,
+    typeDoesntExist,
+  };
 };
 
 const formatDetails = (details: Element[]) =>
-	details.reduce((acc, li) => {
-		const text = li.textContent;
-		if (text === null) return acc;
+  details.reduce((acc, li) => {
+    const text = li.textContent;
+    if (text === null) return acc;
 
-		if (text.includes('CUIT/CUIL'))
-			acc['CUIT/CUIL'] =
-				text.match(/\d+/g) !== null ? text.match(/\d+/g)![0].trim() : 'error';
-		else if (text.includes('CVU'))
-			acc['CVU'] =
-				text.match(/\d+/g) !== null ? text.match(/\d+/g)![0].trim() : 'error';
-		else if (text.includes('@')) acc['mail'] = text.trim();
-		else if (text.includes('CBU')) acc['CBU'] = text.match(/\d+/g)![0].trim();
-		else {
-			if (acc['adicional'] === undefined) acc['adicional'] = [];
-			(acc['adicional'] as (string | number)[]).push(
-				text.replace('Copiar', '').trim(),
-			);
-		}
+    if (text.includes("CUIT/CUIL"))
+      acc["CUIT/CUIL"] =
+        text.match(/\d+/g) !== null ? text.match(/\d+/g)![0].trim() : "error";
+    else if (text.includes("CVU"))
+      acc["CVU"] =
+        text.match(/\d+/g) !== null ? text.match(/\d+/g)![0].trim() : "error";
+    else if (text.includes("@")) acc["mail"] = text.trim();
+    else if (text.includes("CBU")) acc["CBU"] = text.match(/\d+/g)![0].trim();
+    else {
+      if (acc["adicional"] === undefined) acc["adicional"] = [];
+      (acc["adicional"] as (string | number)[]).push(
+        text.replace("Copiar", "").trim()
+      );
+    }
 
-		return acc;
-	}, {} as DetailedMovement['userDetails']);
+    return acc;
+  }, {} as DetailedMovement["userDetails"]);
 
 const getDetailedMovement = async ({
-	page,
-	typeDoesntExist,
-	userInfoDoesntExist,
-	statusDoesntExist,
+  page,
+  typeDoesntExist,
+  userInfoDoesntExist,
+  statusDoesntExist,
 }: {
-	page: Page;
-	typeDoesntExist: boolean;
-	userInfoDoesntExist: boolean;
-	statusDoesntExist: boolean;
+  page: Page;
+  typeDoesntExist: boolean;
+  userInfoDoesntExist: boolean;
+  statusDoesntExist: boolean;
 }) => {
-	const [id, type, status, userName, userDetails] = await Promise.all([
-		page.$eval(movementClassSelectors.id, span =>
-			span.textContent!.match(/\d+/g)![0].trim(),
-		),
-		typeDoesntExist
-			? undefined
-			: page.$eval(movementClassSelectors.type, div => div.textContent),
-		statusDoesntExist
-			? undefined
-			: page.$eval(movementClassSelectors.status, div => div.textContent),
-		userInfoDoesntExist
-			? undefined
-			: page.$eval(movementClassSelectors.userName, li => li.textContent),
-		userInfoDoesntExist
-			? undefined
-			: page.$$eval(movementClassSelectors.userDetails, formatDetails),
-	]);
+  const [id, type, status, userName, userDetails] = await Promise.all([
+    page.$eval(movementClassSelectors.id, (span) =>
+      span.textContent!.match(/\d+/g)![0].trim()
+    ),
+    typeDoesntExist
+      ? undefined
+      : page.$eval(movementClassSelectors.type, (div) => div.textContent),
+    statusDoesntExist
+      ? undefined
+      : page.$eval(movementClassSelectors.status, (div) => div.textContent),
+    userInfoDoesntExist
+      ? undefined
+      : page.$eval(movementClassSelectors.userName, (li) => li.textContent),
+    userInfoDoesntExist
+      ? undefined
+      : page.$$eval(movementClassSelectors.userDetails, formatDetails),
+  ]);
 
-	return {id, type, status, userDetails, userName};
+  return { id, type, status, userDetails, userName };
 };
 
 const getDetailedMovementHandler = async ({
-	browser,
-	movement,
+  browser,
+  movement,
 }: {
-	browser: Browser;
-	movement: Movement;
+  browser: Browser;
+  movement: Movement;
 }): Promise<DetailedMovement | Movement> => {
-	const page = await openMovement({browser, url: movement.url});
-	const pageLoadedCorrectly =
-		(await waitForIdSelector({page})) || (await retryOnError({page}));
-	if (!pageLoadedCorrectly) return movement;
+  const page = await openMovement({ browser, url: movement.url });
+  const pageLoadedCorrectly =
+    (await waitForIdSelector({ page })) || (await retryOnError({ page }));
+  if (!pageLoadedCorrectly) return movement;
 
-	const {statusDoesntExist, userInfoDoesntExist, typeDoesntExist} =
-		await waitForSelectors({page});
+  const { statusDoesntExist, userInfoDoesntExist, typeDoesntExist } =
+    await waitForSelectors({ page });
 
-	const detailedMovement = await getDetailedMovement({
-		page,
-		statusDoesntExist,
-		typeDoesntExist,
-		userInfoDoesntExist,
-	});
-	await page.close();
+  const detailedMovement = await getDetailedMovement({
+    page,
+    statusDoesntExist,
+    typeDoesntExist,
+    userInfoDoesntExist,
+  });
+  await page.close();
 
-	return {...movement, ...detailedMovement};
+  return { ...movement, ...detailedMovement };
 };
 
 const getPageDetailedMovements = async ({
-	pageNumber,
-	page,
-	browser,
+  pageNumber,
+  page,
+  browser,
 }: {
-	pageNumber: number;
-	page: Page;
-	browser: Browser;
+  pageNumber: number;
+  page: Page;
+  browser: Browser;
 }) => {
-	await goToActivities({page, pageNumber});
-	const movements = await getPageMovements({page});
-	return Promise.all(
-		movements.map(movement => getDetailedMovementHandler({browser, movement})),
-	);
+  await goToActivities({ page, pageNumber });
+  const movements = await getPageMovements({ page });
+  return Promise.all(
+    movements.map((movement) =>
+      getDetailedMovementHandler({ browser, movement })
+    )
+  );
 };
 
 export const runner = async ({
-	page,
-	browser,
-	startFromId,
-	maxPage,
-	findMovement,
+  //no tiene sentido que el runner tenga la opcion de cerrarse externamente
+  page,
+  browser,
+  startFromId,
+  maxPage,
+  findMovement,
 }: {
-	page: Page;
-	browser: Browser;
-	startFromId?: string;
-	maxPage?: number;
-} & Omit<MercadoPagoScraperDependencies, 'setMovements'>) => {
-	const detailedMovements: DetailedMovement[] = [];
-	const failedAttempts: Movement[] = [];
+  page: Page;
+  browser: Browser;
+  startFromId?: string;
+  maxPage?: number;
+} & Omit<MercadoPagoScraperDependencies, "setMovements">) => {
+  const detailedMovements: DetailedMovement[] = [];
+  const failedAttempts: Movement[] = [];
 
-	const finalPageNumber = await getFinalPageNumberOnActivities({page});
+  const finalPageNumber = await getFinalPageNumberOnActivities({ page });
 
-	for (
-		let pageNumber = 1,
-			inMaxPage = false,
-			foundLastIndex = false,
-			startSurpassed = false;
-		!foundLastIndex && !startSurpassed && !inMaxPage;
-		pageNumber++
-	) {
-		const pageDetailedMovements = await getPageDetailedMovements({
-			page,
-			browser,
-			pageNumber,
-		});
-		pageDetailedMovements.forEach(movement => {
-			if (movement === null || !('id' in movement)) return;
+  for (
+    let pageNumber = 1,
+      inMaxPage = false,
+      foundLastIndex = false,
+      startSurpassed = false;
+    !foundLastIndex && !startSurpassed && !inMaxPage;
+    pageNumber++
+  ) {
+    const pageDetailedMovements = await getPageDetailedMovements({
+      page,
+      browser,
+      pageNumber,
+    });
+    pageDetailedMovements.forEach((movement) => {
+      if (movement === null || !("id" in movement)) return;
 
-			if (startSurpassed || foundLastIndex) return;
+      if (startSurpassed || foundLastIndex) return;
 
-			if (startFromId !== undefined && movement.id === startFromId) {
-				startSurpassed = true;
-				return detailedMovements.push(movement);
-			}
+      if (startFromId !== undefined && movement.id === startFromId) {
+        startSurpassed = true;
+        return detailedMovements.push(movement);
+      }
 
-			const movementAlreadyExist = findMovement(movement) !== undefined;
-			if (movementAlreadyExist) return (foundLastIndex = true);
+      const movementAlreadyExist = findMovement(movement) !== undefined;
+      if (movementAlreadyExist) return (foundLastIndex = true);
 
-			return detailedMovements.push(movement);
-		});
+      return detailedMovements.push(movement);
+    });
 
-		if (maxPage !== undefined && maxPage === pageNumber) inMaxPage = true;
-		if (finalPageNumber === pageNumber) foundLastIndex = true;
-	}
+    if (maxPage !== undefined && maxPage === pageNumber) inMaxPage = true;
+    if (finalPageNumber === pageNumber) foundLastIndex = true;
+  }
 
-	await browser.close();
-	return detailedMovements;
+  // await browser.close();
+  return detailedMovements;
 };
